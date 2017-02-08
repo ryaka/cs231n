6fa6b190acbf7de0fc4329a7860ca43108d2b96f
Rhea Kara <hello@rheakara.com>
2017-02-08 00:28:01 -0800

    Complete assignment 1 svm

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -20,7 +20,6 @@ def svm_loss_naive(W, X, y, reg):
   - gradient with respect to weights W; an array of same shape as W
   """
   dW = np.zeros(W.shape) # initialize the gradient as zero
-
   # compute the loss and the gradient
   num_classes = W.shape[1]
   num_train = X.shape[0]
@@ -34,6 +33,8 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        dW[:, j] += X[i].T
+        dW[:, y[i]] -= X[i].T
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
@@ -42,15 +43,17 @@ def svm_loss_naive(W, X, y, reg):
   # Add regularization to the loss.
   loss += 0.5 * reg * np.sum(W * W)
 
+  dW /= num_train
+  # Add regularization to the gradient.
+  dW += reg * W
+
   #############################################################################
-  # TODO:                                                                     #
   # Compute the gradient of the loss function and store it dW.                #
   # Rather that first computing the loss and then computing the derivative,   #
   # it may be simpler to compute the derivative at the same time that the     #
   # loss is being computed. As a result you may need to modify some of the    #
   # code above to compute the gradient.                                       #
   #############################################################################
-
 
   return loss, dW
 
@@ -61,22 +64,41 @@ def svm_loss_vectorized(W, X, y, reg):
 
   Inputs and outputs are the same as svm_loss_naive.
   """
-  loss = 0.0
-  dW = np.zeros(W.shape) # initialize the gradient as zero
+
+  num_train = X.shape[0]
 
   #############################################################################
-  # TODO:                                                                     #
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+
+  # A matrix containing all predicted values for each sample.
+  # X[i][j] represents the predicted value for sample i being class j.
+  preds = X.dot(W)
+
+  # For each sample (row), get the actual classes' predicted value.
+  correct_class_scores = preds[range(num_train), y]
+
+  # Add the delta to each prediction's cost.
+  delta = 1
+  margins = np.maximum(0, (preds.T - correct_class_scores).T + delta)
+
+  # Zero out when j == y[i]
+  margins[range(num_train), y] = 0
+
+
+  loss = np.sum(margins)
+  loss /= num_train
+
+  # Add regularization to the loss.
+  loss += 0.5 * reg * np.sum(W * W)
+
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
 
 
   #############################################################################
-  # TODO:                                                                     #
   # Implement a vectorized version of the gradient for the structured SVM     #
   # loss, storing the result in dW.                                           #
   #                                                                           #
@@ -84,7 +106,21 @@ def svm_loss_vectorized(W, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
-  pass
+
+  # Get number of class predictions that did not fit the margin for each sample
+  num_margins = np.sum(margins > 0, axis=1)
+
+  # Each point (i, j) represents the number of times to add X.T[i] to dW[:, j]
+  #
+  # Each point (i, y[i]) is equal to the negative sum for its row
+  margins_occurr = (margins > 0) * 1
+  margins_occurr[range(num_train), y] = -num_margins
+
+  dW = X.T.dot(margins_occurr)
+  dW /= num_train
+  # Add regularization to the gradient.
+  dW += reg * W
+
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
